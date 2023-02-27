258a7c139f0c5cefcaf5a8e3107fd22c78db8642
Andrei Karlin <andreikarlin@gmail.com>
2023-02-27 10:14:37 -0500

    create class
    probably doesn't work

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList
 
+public class NoNullArrayList<T> extends ArrayList<T> {
+  NoNullArrayList<T>() {
+    super<T>()
+  }
+  NoNullArrayList<T>(int initialCapacity) {
+    super<T>(initialCapacity)
+  }
+  
+  public void add(T object) {
+    if (object == null) 
+      return IllegalArgumentException
+    super.add(object)
+  }
+  public bool add(T object, int index) {
+    if (object == null) 
+      return IllegalArgumentException
+    return super.add(object, index)
+  }
+  public T set(T object, int index) {
+    if (object == null) 
+      return IllegalArgumentException
+    return super.set(object, index)
+  }
+}
