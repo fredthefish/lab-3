b68833a25c37bc7c80d8c47c7fe7c80d48f498e7
Andrei Karlin <andreikarlin@gmail.com>
2023-03-02 23:38:59 -0500

    worked on this lab

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,1 +1,24 @@
-
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+    OrderedArrayList() {
+        super();
+    }
+    OrderedArrayList(int initialCapacity) {
+        super(initialCapacity);
+    }
+    public boolean add(T object) {
+        for(int i = 0; i < this.length; i++) {
+            if (object.compareTo(get(i)) > 0) {
+                super.add(i, object);
+                return true;
+            }
+        }
+    }
+    public void add(int index, T object) {
+        add(object); //The index doesn't matter.
+    }
+    public T set(int index, T object) {
+        T original = super.remove(index);
+        add(object);
+        return original;
+    }    
+}
